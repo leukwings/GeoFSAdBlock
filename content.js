f91fe821d562e9325a6f76d4ae13d161e0ad7e49
leukwings <110693106+leukwings@users.noreply.github.com>
2024-10-27 17:51:46 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,14 +1,10 @@
-// Funktion zum Entfernen der Werbung
 function removeAds() {
-    // Selektor für das Werbungselement
-    const adSelector = ".geofs-adbanner"; // Dies ist der Selektor, der die Werbung identifiziert
+    const adSelector = ".geofs-adbanner";
     
-    // Suche nach allen Werbeelementen und entferne sie
     const ads = document.querySelectorAll(adSelector);
     ads.forEach(ad => {
         ad.remove();
     });
 }
 
-// Warte, bis die Seite vollständig geladen ist, und führe die Funktion aus
 window.addEventListener('load', removeAds);
